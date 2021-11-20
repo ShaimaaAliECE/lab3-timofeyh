b208d7b57730557940b261d5c555862461863a76
timofeyh <timofeyhartanovich@gmail.com>
2021-11-19 19:21:13 -0500

    changed path

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
     console.log("Server Accessed");
-    console.log(path.join(__dirname, 'build', 'index.html'))
+    console.log(path.join(__dirname, './client/app', 'index.html'))
     res.send();
 });
 
