0ae1dd0e27180dcd7426d469ff5b2883b07367f4
timofeyh <timofeyhartanovich@gmail.com>
2021-11-20 18:32:59 -0500

    new sql instance (changed ip)

--- server/connect_db.js
+++ server/connect_db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 
 function newConnection() {
     let conn = mysql.createConnection({
-        host:'104.197.55.101',
+        host:'104.198.141.4',
         user: 'root',
         password: 'root',
         database: 'skedul_db'
